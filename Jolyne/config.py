83f1c23917a23801acc5ad735070e6374a8ced26
bakuzan <imannoyed4days@hotmail.co.uk>
2019-04-18 10:36:24 +0100

    working heroku local.

--- Jolyne/config.py
+++ Jolyne/config.py
@@ -13,24 +13,24 @@ def load_config():
     config = Config()
 
     config.reddit = {
-        client_id: environ.get('REDDIT_CLIENT_ID'),
-        client_secret: environ.get('REDDIT_CLIENT_SECRET'),
-        password: environ.get('REDDIT_PASSWORD'),
-        username: environ.get('REDDIT_USERNAME'),
-        user_agent: environ.get('REDDIT_USER_AGENT')
+        'client_id': environ.get('REDDIT_CLIENT_ID'),
+        'client_secret': environ.get('REDDIT_CLIENT_SECRET'),
+        'password': environ.get('REDDIT_PASSWORD'),
+        'username': environ.get('REDDIT_USERNAME'),
+        'user_agent': environ.get('REDDIT_USER_AGENT')
     }
 
     config.options = {
-        subreddits: environ.get('OPT_SUBREDDITS'),
-        comment_limit: environ.get('OPT_COMMENT_LIMIT'),
-        search_start_hours_ago: environ.get('OPT_SEARCH_START_HOURS_AGO')
+        'subreddits': environ.get('OPT_SUBREDDITS'),
+        'comment_limit': environ.get('OPT_COMMENT_LIMIT'),
+        'search_start_hours_ago': environ.get('OPT_SEARCH_START_HOURS_AGO')
     }
 
     config.db = {
-        user: environ.get('DB_USER'),
-        password: environ.get('DB_PASSWORD'),
-        port: environ.get('DB_PORT'),
-        name: environ.get('DB_NAME')
+        'user': environ.get('DB_USER'),
+        'password': environ.get('DB_PASSWORD'),
+        'port': environ.get('DB_PORT'),
+        'name': environ.get('DB_NAME')
     }
 
     return config
